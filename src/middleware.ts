3535da8c2ec1377d1f0a453680f3b0e43ee1861d
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-08-27 11:25:51 -0300

    🐛 fix: Permite acesso à rota de logout sem autenticação
    - Adiciona a rota /api/logout à lista de páginas públicas.
    - Corrige o problema que impedia o logout do usuário.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import AuthService from "./modules/auth/service/auth-service";
 
 export const config = {
-  matcher: "/((?!_next|favicon.ico|public|.*\\..*).*)",
+  matcher: "/((?!_next|favicon.ico|public|.*\\..*).*)"
 };
 
-const publicPages = ["/login"];
+const publicPages = ["/login", "/api/logout"];
 
 export async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname
+  const pathname = req.nextUrl.pathname;
 
-  if(publicPages.includes(pathname)){
-      return NextResponse.next()
+  if (publicPages.includes(pathname)) {
+    return NextResponse.next();
   }
 
-  const session = await AuthService.isSessionValid()
+  const session = await AuthService.isSessionValid();
 
-  if(!session){
-      const isAPIRoute = pathname.startsWith('/api')
+  if (!session) {
+    const isAPIRoute = pathname.startsWith("/api");
 
-      if(isAPIRoute){
-          return NextResponse.json({ message:'Unauthorized' }, { status: 401 })
-      }
-      return NextResponse.redirect(new URL('/login', req.url))
+    if (isAPIRoute) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
